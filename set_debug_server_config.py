95987fc5f30d23a62e6cea08e15c158a2869ac66
Leonardo Hax Damiani <lhdamiani@gmail.com>
2019-04-26 13:22:16 +0200

    Script to start local system. Debug server configuration script.

---
+++ set_debug_server_config.py
@@ -0,0 +1,39 @@
+from detector_integration_api import DetectorIntegrationClient
+
+
+# Address of our api server.
+api_address = "http://0.0.0.0:10000"
+
+# Initialize the client.
+client = DetectorIntegrationClient(api_address)
+
+# Get the status of the integration.
+status = client.get_config()
+
+# Check if the integration is in the INITIALIZED state.
+if status != "IntegrationStatus.INITIALIZED":
+    # Resetting the integration will bring us to the IntegrationStatus.INITIALIZED state.
+    client.reset()
+
+# Define the config for the writer.
+writer_config = {"output_file": "/tmp/test.h5",
+                 "n_frames": 100,
+                 "user_id": 0}
+
+# Define the config for the backend.
+backend_config = {"bit_depth": 16,
+                  "n_frames": 100}
+
+# Define the config for the detector.
+detector_config = {"period": 0.1,
+                   "frames": 100,
+                   "exptime": 0.01,
+                   "dr": 16}
+                   
+configuration = {"writer": writer_config,
+                 "backend": backend_config,
+                 "detector": detector_config}
+
+# Send the config to the writer, backend and detector. 
+# This changes the integration to IntegrationStatus.CONFIGURED state
+client.set_config(configuration)
---
+++ start_locally.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+# opens new tab
+new_tab_right(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "d" using command down' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "cd Software/diaui/polymer"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+new_tab_bottom(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "d" using {command down, shift down}' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "cd Software/diaui"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+# loads the conda 
+load_conda(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "source ~/anaconda3/etc/profile.d/conda.sh"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+# activate diaui environment
+activate_diaui(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "conda activate /anaconda3/envs/diaui"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+#starts python flask server
+start_flaskserver(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "python server.py"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+# starts the bsread stream
+start_bsread_stream(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "python bsread_stream/start_stream.py"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+# starts the detector intergration debug server
+start_detector_debug_server(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "python detector_integration_api/build/lib/detector_integration_api/start_debug_server.py"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+sets_debug_server_config(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "python set_debug_server_config.py"' \
+    -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}   
+
+move_focus_to_top(){
+    osascript \
+    -e 'tell application "iTerm" to activate' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "[" using {command down}' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "[" using {command down}' \
+    -e 'tell application "System Events" to tell process "iTerm" to keystroke "[" using {command down}' 
+}   
+
+load_conda
+activate_diaui
+new_tab_bottom
+load_conda
+activate_diaui
+start_detector_debug_server
+new_tab_right
+load_conda
+activate_diaui
+sets_debug_server_config
+start_bsread_stream
+new_tab_bottom
+load_conda
+activate_diaui
+start_flaskserver
+move_focus_to_top
