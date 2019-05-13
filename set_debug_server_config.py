8d1df64297a2820ed74839dcc909783218f491fc
Leonardo Hax Damiani <lhdamiani@gmail.com>
2019-05-13 17:35:30 +0200

    Script to set the initial configuration for debug server.

--- set_debug_server_config.py
+++ set_debug_server_config.py
@@ -8,17 +8,17 @@ api_address = "http://0.0.0.0:10000"
 client = DetectorIntegrationClient(api_address)
 
 # Get the status of the integration.
-status = client.get_config()
+status = client.get_status()
 
-# Check if the integration is in the INITIALIZED state.
-if status != "IntegrationStatus.INITIALIZED":
+# Check if the integration is in the READY state.
+if status != "IntegrationStatus.READY":
     # Resetting the integration will bring us to the IntegrationStatus.INITIALIZED state.
     client.reset()
 
 # Define the config for the writer.
 writer_config = {"output_file": "/tmp/test.h5",
                  "n_frames": 100,
-                 "user_id": 0}
+                 "user_id": 10001}
 
 # Define the config for the backend.
 backend_config = {"bit_depth": 16,
@@ -28,6 +28,8 @@ backend_config = {"bit_depth": 16,
 detector_config = {"period": 0.1,
                    "frames": 100,
                    "exptime": 0.01,
+                   "cycles": 100,
+                   "timing": "gating",
                    "dr": 16}
                    
 configuration = {"writer": writer_config,
