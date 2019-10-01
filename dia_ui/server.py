f7e093bd3933b65d81dc3323dac0573ae1d0cc03
Leonardo Hax Damiani <lhdamiani@gmail.com>
2019-10-01 08:57:25 +0200

    os system with sleep to generate log.

--- dia_ui/server.py
+++ dia_ui/server.py
@@ -219,9 +219,9 @@ def get_diaLog(json, methods=['GET', 'POST']):
         # runs the service that converts the journalctl to a txt file
         import subprocess
         from pathlib import Path
-        cline = "sudo journalctl -u dia.service > /home/dia_ui/dia.log"
-        prog = subprocess.Popen(cline, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        out, err = prog.communicate()
+        # subprocess.call(["sudo journalctl -u dia.service > /home/dia_ui/dia.log"])
+        os.system("sudo journalctl -u dia.service > /home/dia_ui/dia.log")  
+        time.sleep(2)
         dia_log_file = Path("/home/dia_ui/dia.log")
     except Exception as e:
         # emits problem
